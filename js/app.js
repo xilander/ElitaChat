39c2d4e0d66de5b7162f1fae82faad8c8925f0e6
Xilander <xilander@hotmail.com>
2021-05-02 19:40:21 -0500

    Modificacion de ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+var url = window.location.href;
+var swLocation = '/ElitaChat/sw.js'
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swLocation = '/sw.js';
+    }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
